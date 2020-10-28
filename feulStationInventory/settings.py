3f362bcf1a37809e290fbae7cc685e0d2346e62f
AniekutmfonGodwin <adriano8263@gmail.com>
2020-10-28 22:41:42 +0100

    ready to submit

--- feulStationInventory/settings.py
+++ feulStationInventory/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '0*y0i2$y2+xi827re-l-f(x36!*iifb7ydxrnq5t^ml38u86n#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
